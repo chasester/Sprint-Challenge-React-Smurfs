8dd036e03f86f85728e945151baa35dd149c2498
Chase Wex <clapingmanly@hotmail.com>
2019-05-10 12:04:16 -0400

    Added call back function for adding smurfs  and added routes for main page and form

--- village/src/App.js
+++ village/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect} from "react-router-dom";
 import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import Smurf from './components/Smurf';
 
 class App extends Component {
   constructor(props) {
@@ -20,15 +22,24 @@ class App extends Component {
     .catch(err=> console.log(err));
   }
 
+  addSmurf(obj)
+  {
+    axios.post(`http://localhost:3333/smurfs`, obj)
+    .then((res)=> this.setState({smurfs: res.data}))
+    .catch(err=> console.log(err));
+  }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
-      <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
-      </div>
+      <Router>
+        <div className="App">
+          <Route path="/form" exact render={(props) => <SmurfForm {...props} submitCb={(obj) => this.addSmurf(obj)}/>}/>
+          <Route path="/" exact render={(props) => <Smurfs {...props} smurfs={this.state.smurfs} />}/>
+         
+        </div>
+      </Router>
     );
   }
 }
